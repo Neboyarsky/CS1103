6f3995afc82820760c571693e17168df36a0c2aa
neboyarsky <1833Yt,jzhcrb>
2019-06-25 15:15:54 +0300

    practiced in binary search and arrays of objects

---
+++ src/RA_W1/AL_Exp.java
@@ -0,0 +1,32 @@
+package RA_W1;
+
+import java.util.ArrayList;
+
+public class AL_Exp {
+    public static void main(String[] args) {
+        ArrayList list;
+        list = new ArrayList<Integer>();
+
+        list.add(7);
+
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println("Here is your " + i + " element of array: " + list.get(i));
+        }
+
+
+        list.add(8);
+
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println("Here is your " + i + " element of array: " + list.get(i));
+        }
+
+        list.add(9);
+
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println("Here is your " + i + " element of array: " + list.get(i));
+        }
+
+
+
+    }
+}
---
+++ src/RA_W1/Searches/Contact.java
@@ -0,0 +1,25 @@
+package RA_W1.Searches;
+
+public class Contact {
+    private final String firstName;
+
+    public Contact(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Contact() {
+        this.firstName = "defaultFirstName";
+        this.lastName = "defaultLastName";
+    }
+
+    private final String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+}
