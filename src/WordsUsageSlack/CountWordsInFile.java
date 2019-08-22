5556c8461a9ea828ad2398559f9617e651da0455
michael.genenko <genenko@gmail.com>
2019-08-22 14:22:42 +0300

    fixed an error that lead to reading the name of the file instead of its content https://www.youtube.com/watch?v=UgECyPae8pw

--- src/WordsUsageSlack/CountWordsInFile.java
+++ src/WordsUsageSlack/CountWordsInFile.java
@@ -2,14 +2,13 @@ package WordsUsageSlack;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.PrintWriter;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
 class CountWordsInFile {
-    void countEachWord(String fileName, Map<String, Integer> words) throws FileNotFoundException {
-        File src;
-        Scanner file = new Scanner(fileName);
+    static void countEachWord(String fileName, Map<String, Integer> words) throws FileNotFoundException {
+        Scanner file = new Scanner(new File(fileName));
         while (file.hasNext()) {
             String word = file.next();
             Integer count = words.get(word);
@@ -20,5 +19,11 @@ class CountWordsInFile {
                 count = 1;
             words.put(word, count);
         }
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        Map<String, Integer> words = new HashMap<String, Integer>();
+        countEachWord("/users/michael.genenko/desktop/support-escalations/2019-01-01.txt", words);
+        System.out.println(words);
     }
 }
